c3e35341ff505b12ecfc4913182129a40668f96e
RebelPilot89 <marcocrush@gmail.com>
2024-12-18 00:36:14 -0600

    Tree_device
    From cuscu whit android 15

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for motorola motorola edge 50 fusion (cusco)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_cusco.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_cusco-user \
+    omni_cusco-userdebug \
+    omni_cusco-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for motorola motorola edge 50 fusion (cusco)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_cusco-user
+add_lunch_combo omni_cusco-userdebug
+add_lunch_combo omni_cusco-eng
